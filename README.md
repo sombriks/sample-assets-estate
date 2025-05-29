d1b15693c561e336acad780bf35b82e0571c2024
Leonardo Silveira <sombriks@gmail.com>
2025-05-28 21:16:10 -0300

    some templating

--- README.md
+++ README.md
@@ -2,16 +2,16 @@
 
 [![Java CI with Maven](https://github.com/sombriks/sample-assets-estate/actions/workflows/maven.yml/badge.svg)](https://github.com/sombriks/sample-assets-estate/actions/workflows/maven.yml)
 
-Small experiment
+Small experiment on several things
 
 ## Requirements
 
-- java 24
-- maven 3.9
-- spring boot 3.5
-- sqlite 3
-- htmx 2
-- pug4j 2 
+- [java 24][java]
+- [maven 3.9][maven]
+- [spring boot 3.5][spring-boot]
+- [sqlite 3][sqlite]
+- [htmx.org 2][htmx]
+- [pug4j 2][pug] 
 
 ## Initial setup
 
@@ -34,5 +34,10 @@ Small experiment
 - 
 
 [repo]: https://github.com/sombriks/sample-assets-estate
+[java]: https://dev.java
+[maven]: https://maven.apache.org
+[spring-boot]: https://spring.io/projects/spring-boot
+[sqlite]: https://sqlite.org
+[htmx]: https://htmx.org
+[pug]: https://github.com/neuland/pug4j
 [initializr]: https://start.spring.io/#!type=maven-project&language=java&platformVersion=3.5.0&packaging=jar&jvmVersion=24&groupId=sample&artifactId=assets-estate&name=assets-estate&description=Demo%20project%20for%20Spring%20Boot&packageName=sample.assets.estate&dependencies=devtools,liquibase,data-jpa,web
-
--- src/main/resources/changelogs/2025-05-27-initial-schema.sql
+++ src/main/resources/changelogs/2025-05-27-initial-schema.sql
@@ -1,12 +1,38 @@
 -- liquibase formatted sql
 
--- changeset sombriks:2025-05-27-initial-schema.sql
+-- changeset sombriks:2025-05-27-initial-schema.sql:1
 
-create table assets(
-    id integer not null primary key  autoincrement,
-    name text not null,
+create table asset_types
+(
+    id      integer   not null primary key autoincrement,
+    name    text      not null unique,
     created timestamp not null default current_timestamp,
     updated timestamp
 );
 
+-- rollback drop table asset_types
+
+-- changeset sombriks:2025-05-27-initial-schema.sql:2
+
+create table assets
+(
+    id             integer   not null primary key autoincrement,
+    name           text      not null,
+    asset_types_id integer   not null references asset_types (id),
+    created        timestamp not null default current_timestamp,
+    updated        timestamp
+);
+
 -- rollback drop table assets
+
+-- changeset sombriks:2025-05-27-initial-schema.sql:3
+
+create table groups
+(
+    id             integer   not null primary key autoincrement,
+    name           text      not null,
+    created        timestamp not null default current_timestamp,
+    updated        timestamp
+);
+
+-- rollback drop table groups
