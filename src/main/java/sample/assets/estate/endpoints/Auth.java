d16d630035b29c56e91224656b8b784fef859710
Leonardo Silveira <sombriks@gmail.com>
2025-06-01 15:33:51 -0300

    pug components

--- src/main/java/sample/assets/estate/endpoints/Auth.java
+++ src/main/java/sample/assets/estate/endpoints/Auth.java
@@ -4,14 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 import sample.assets.estate.service.AccessService;
 
 @Controller
 @RequestMapping("/auth")
 public class Auth {
 
-    private static final Logger LOG =  LoggerFactory.getLogger(Auth.class);
+    private static final Logger LOG = LoggerFactory.getLogger(Auth.class);
 
     private final AccessService accessService;
 
@@ -21,6 +24,14 @@ public class Auth {
 
     @GetMapping
     public String index() {
+        LOG.info("Accessing the auth page");
         return "pages/auth";
+    }
+
+    @PutMapping
+    public ModelAndView signIn(String email, String password) {
+        LOG.info("Signing in with email: {}", email);
+        LOG.info("Signing in");
+        return new ModelAndView("/");
     }
 }
--- src/main/java/sample/assets/estate/endpoints/Index.java
+++ src/main/java/sample/assets/estate/endpoints/Index.java
@@ -1,22 +1,19 @@
 package sample.assets.estate.endpoints;
 
-import de.neuland.pug4j.spring.view.PugView;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.View;
-import org.springframework.web.servlet.view.RedirectView;
+
 import sample.assets.estate.service.AccessService;
 
 @Controller
 @RequestMapping("/")
 public class Index {
 
-    private static final Logger LOG =  LoggerFactory.getLogger(Index.class);
+    private static final Logger LOG = LoggerFactory.getLogger(Index.class);
 
     private final AccessService accessService;
 
@@ -25,10 +22,8 @@ public class Index {
     }
 
     @GetMapping
-    public ModelAndView index(@RequestHeader(name = "X-Auth-Token", required = false) String token) {
-        if(accessService.valid(token))
-            return new ModelAndView("pages/index");
-        else
-            return new ModelAndView("redirect:/auth");
+    public ModelAndView index() {
+        LOG.info("index page");
+        return new ModelAndView("pages/index");
     }
 }
